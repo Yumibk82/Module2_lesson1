65376701952b586b116481e3cb3b13c42ef559dd
Hang Tran <ttthang0102@gmail.com>
2021-01-08 08:57:29 +0700

    lesson 1

---
+++ src/Lesson1_test1.java
@@ -0,0 +1,13 @@
+import java.util.Date;
+public class Lesson1_test1 {
+
+    public static void main(String[] args) {
+        int x = 7;
+        if (x > 9) {
+            System.out.println("case1");
+        } else if (x < 6) {
+            System.out.println("case2");
+    }
+    }
+
+}
